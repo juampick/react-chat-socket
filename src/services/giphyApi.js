028de979ccd0c7854237c3391c7cbf5c567ba79e
Juan P. Cook <juampick@gmail.com>
2021-05-05 13:00:22 -0300

    Catching and error handling

--- src/services/giphyApi.js
+++ src/services/giphyApi.js
@@ -1,5 +1,5 @@
 const GIPHY_BASE_API_PATH = process.env.GIPHY_BASE_API_PATH;
-const GIPHY_API_KEY = process.env.GIPHY_API_KEY;
+const GIPHY_API_KEY = process.env.GIPHY_API_KEY+'dsdsd';
 
 /**
  * Searchs using GiphyAPI and returns the first gif found based on the provided search query
@@ -9,14 +9,22 @@ const GIPHY_API_KEY = process.env.GIPHY_API_KEY;
  */
 export const getGiphyGifByQuery = async (query, limit = 10) => {
     const buildURL = `${GIPHY_BASE_API_PATH}/search?q=${query}&api_key=${GIPHY_API_KEY}&limit=${limit}`;
+    const EMPTY_URL = '';
 
     return await fetch(buildURL)
-                .then(res => res.json())
                 .then(res => {
-                    if (res.meta.status === 200){
+                    if (!res.ok) return EMPTY_URL;
+                    return res.json();
+                })
+                .then(res => {
+                    if (res.meta && res.meta.status === 200){
                         if (res.data.length){
-                            return res.data[0] ? res.data[0].images['fixed_height_small'].url : ''
+                            return res.data[0] ? res.data[0].images['fixed_height_small'].url : EMPTY_URL
                         }
                     }
+                    return EMPTY_URL;
+                })
+                .catch(() => {
+                    return EMPTY_URL;
                 });
 }
